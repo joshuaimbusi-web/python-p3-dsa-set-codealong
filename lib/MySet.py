7fb76d22ab991c1ca9307e7c39bb3096cb25a9c3
joshuaimbusi <joshua.imbusi@student.moringaschool.com>
2025-11-30 17:44:05 +0300

    "initial commit"

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,29 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True 
+        return self                   
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary = {}
+        return self
+    
+    def __str__(self):
+        content = ",".join(str(key) for key in self.dictionary.keys())
+        return f"MySet: {{{content}}}"
+
+        
